5b0a7c5d1972b8a5f72aae97b04eeae60e658399
Khushi Jain <khushi.jain0704@gmail.com>
2021-06-16 20:11:26 +0530

    initial staging

---
+++ tutorial/LoginSystem.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream> //contains both ofstream & ifstream operations
+#include <string>
+using namespace std;
+class UserLogin{
+    private:
+    string username="khushijain@gmail.com";
+    string password="khushi";
+    string userAttempted;
+    string passwordAttempted;
+    public:
+    UserLogin(){
+        // constructor for creating objects
+    }
+    void login(){
+        cout<<" Please enter the following details to get access to the files :\n";
+        cout<<"username : \n";
+        cin>>userAttempted;
+    // before moving ahead with password lets check if the email exists in the database
+        if(userAttempted == username){
+            // move ahead , does nothing for now
+        }
+        else{
+            cout<<"\nOops! Invalid user id\n"; 
+            login();
+        }
+        
+        cout<<"password : \n";
+        cin>>passwordAttempted;
+        if(passwordAttempted == password){
+            cout<<"\nBingo ! You now have access to the files\n";
+            // does smtg like opens a file
+        }
+        else{
+           cout<<"\nOops! Incorrect password\n"; 
+        }
+    }
+};
+int main(){
+    UserLogin app;
+    app.login();
+    return 0; 
+}
