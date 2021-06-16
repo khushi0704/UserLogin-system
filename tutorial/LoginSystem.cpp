3d75ac72ab21770c724a2cdc210281fb7c7fd674
Khushi Jain <khushi.jain0704@gmail.com>
2021-06-16 23:52:44 +0530

    updated

--- tutorial/LoginSystem.cpp
+++ tutorial/LoginSystem.cpp
@@ -4,13 +4,30 @@
 using namespace std;
 class UserLogin{
     private:
-    string username="khushijain@gmail.com";
-    string password="khushi";
+    string username;
+    string password;
     string userAttempted;
     string passwordAttempted;
+    bool access = false;
     public:
     UserLogin(){
         // constructor for creating objects
+    }
+    void check_file(){
+        string uline,pline;
+        fstream file;
+        file.open("user.txt",ios::in);
+        if(file.is_open()){
+            getline(file,uline);
+            username=uline;
+        }
+        file.close();
+        file.open("password.txt",ios::in);
+        if(file.is_open()){
+            getline(file,pline);
+            password=pline;
+        }
+        file.close();
     }
     void login(){
         cout<<" Please enter the following details to get access to the files :\n";
@@ -18,27 +35,30 @@ class UserLogin{
         cin>>userAttempted;
     // before moving ahead with password lets check if the email exists in the database
         if(userAttempted == username){
-                // move ahead , does nothing for now
+            // move ahead , does nothing for now
+            while(!access){
             cout<<"password : \n";
             cin>>passwordAttempted;
             if(passwordAttempted == password){
                 cout<<"\nBingo ! You now have access to the files\n";
+                //return the file content
+                access=true;
                 // does smtg like opens a file
             }
             else{
                cout<<"\nOops! Incorrect password\n"; 
             }
+            }
         }
         else{
             cout<<"\nOops! Invalid user id\n"; 
             login();
-        }
-        
-        
+        } 
     }
 };
 int main(){
     UserLogin app;
+    app.check_file();
     app.login();
     return 0; 
 }
