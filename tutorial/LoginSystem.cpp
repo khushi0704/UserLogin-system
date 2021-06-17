460e3687976210ae8f6f497b757204bc6643bc2d
Khushi Jain <khushi.jain0704@gmail.com>
2021-06-17 22:40:57 +0530

    updated

--- tutorial/LoginSystem.cpp
+++ tutorial/LoginSystem.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream> //contains both ofstream & ifstream operations
 #include <string>
+#include <cstdio>
 using namespace std;
 class UserLogin{
     private:
@@ -29,29 +30,59 @@ class UserLogin{
         }
         file.close();
     }
+    // displays file content
+    void show_file()
+    {
+
+        string line;
+        fstream showfile;
+        showfile.open("target.txt",ios::in);
+        if(!showfile)
+        {
+                cout<<"\nError in opening file..!!\n";
+
+                exit(0);
+        }
+
+        cout<<"\n";
+        while(getline(showfile,line))
+    {
+        cout<<line<<"\n";
+    }
+        cout<<"\n";
+        showfile.close();
+    }
     void login(){
         cout<<" Please enter the following details to get access to the files :\n";
         cout<<"username : \n";
         cin>>userAttempted;
-    // before moving ahead with password lets check if the email exists in the database
-        if(userAttempted == username){
+        // before moving ahead with password lets check if the email exists in the database
+        if(userAttempted == username)
+        {
             // move ahead , does nothing for now
-            while(!access){
+            while(!access)
+            {
             cout<<"password : \n";
             cin>>passwordAttempted;
-            if(passwordAttempted == password){
-                cout<<"\nBingo ! You now have access to the files\n";
-                //return the file content
-                access=true;
-                // does smtg like opens a file
-            }
-            else{
-               cout<<"\nOops! Incorrect password\n"; 
-            }
+                if(passwordAttempted == password)
+                {
+                    char file_name[20];
+                    cout<<"\nBingo ! You now have access to the files\n";
+                    cout<<"Enter the file name you want to look at : \n";
+                    cin>>file_name;
+                    //return the file content
+                    show_file();
+                    access=true;
+                    // does smtg like opens a file
+                }
+                else
+                {
+                   cout<<"\nOops! Incorrect password\n"; 
+                }
             }
         }
         else{
-            cout<<"\nOops! Invalid user id\n"; 
+            cout<<"\nOops! Invalid user id\n"<<"Your id is not registered in our Database"; 
             login();
         } 
     }
@@ -60,5 +91,5 @@ int main(){
     UserLogin app;
     app.check_file();
     app.login();
-    return 0; 
+    return 0;  
 }
